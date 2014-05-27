62ed6138505774fbfd49853c43e65697b1012fcb
tumada <taka.umada@hotmail.com>
2014-05-27 12:08:05 +0000

    commit

---
+++ public/main.js
@@ -0,0 +1,20 @@
+/// <reference path="../node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js" />
+/// <reference path="../typings/d3/d3.d.ts" />
+/// <reference path="../typings/jquery/jquery.d.ts" />
+
+
+var socket = io.connect('http://tumadademo61.azurewebsites.net/');
+socket.on('connect');
+
+
+$('#messageBox').change(function(){
+	socket.emit('vote', JSON.stringify({
+		message: $('#messageBox').val()
+	}))
+	$('#messageBox').val('');
+});
+
+socket.on('refresh', function(data) {
+	var jData = JSON.parse(data);
+	$('#chat').prepend(jData.message + '<br />');
+});
